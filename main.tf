1530bb883f5791f26040af8e761196473d67a259
sturms_robot <151828922+sturmsrobot@users.noreply.github.com>
2024-08-01 14:29:49 +0200

    korrektur main.tf

--- main.tf
+++ main.tf
@@ -13,10 +13,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  region = "eu-central-1"
-}
-
 # Erstelle eine VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
--- provider.tf
+++ provider.tf
@@ -1,4 +1,3 @@
-# provider.tf
 provider "aws" {
   region = "eu-central-1" # Frankfurt Region
 }
